5bd9986b39146916b2f6f925a33782266544ffcb
Virag Zaveri <viragz18@gmail.com>
2021-11-27 23:46:12 -0500

    Adding Java folder and problem solved on 11/27/21

---
+++ Java/HackerRank/counting_valleys.java
@@ -0,0 +1,94 @@
+package HackerRank;
+/*
+An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
+
+A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+Example
+
+ Steps = 8, path = [DDUUUUDD]
+
+The hiker first enters a valley  units deep. Then they climb out and up onto a mountain  units high. Finally, the hiker returns to sea level and ends the hike.
+
+Function Description
+
+Complete the countingValleys function in the editor below.
+
+countingValleys has the following parameter(s):
+
+int steps: the number of steps on the hike
+string path: a string describing the path
+Returns
+
+int: the number of valleys traversed
+
+
+Sample Input:
+    8
+    UDDDUDUU
+O/p: 1
+*/
+
+
+// Problem from Hackerrank - Interview preparation kit - Warm up challenge
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+    // Write your code here
+        int valley = 0, D = 0; 
+        for(char ch : path.toCharArray() ){
+            if(ch == 'U'){
+                D--;
+            }
+            else {
+                D++;
+            }
+            
+            if(ch == 'U' && D == 0) valley ++;
+        }
+        return valley;
+
+    }
+
+}
+
+/*
+    The trick here was just to identify the sea level D==0
+    Increase the valley count when reach the sea level D =0 only when the last step was take is uphill
+    In code terms
+
+    if(D==0 && ch == 'U')
+*/
+
+public class counting_valleys {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
